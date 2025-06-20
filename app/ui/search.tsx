4db26e21a18b5822b07d17a8541e52f24efe2a86
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-20 11:49:23 -0500

    Refactor Search component to improve search functionality and handle input changes

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,26 @@
-'use client';
-
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+"use client";
+import { useState } from "react";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearch() {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", "1"); // Reset to the first page on search
+    if (searchTerm) {
+      params.set("query", searchTerm);
+    } else {
+      params.delete("query");
+    }
+    // Update the URL with the new search term
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +29,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
+      <MagnifyingGlassIcon
+        className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"
+        onClick={() => handleSearch()}
+      />
     </div>
   );
 }
