97ecddd8c6d3aa28b53b9440898ba392060ab45c
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-20 14:47:12 -0500

    Refactor invoice actions and add updateInvoice function for modifying existing invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, {
@@ -38,5 +38,27 @@ export async function createInvoice(formData: FormData) {
   `;
 
   revalidatePath("/dashboard/invoices");
-  redirect('/dashboard/invoices');
+  redirect("/dashboard/invoices");
+}
+
+
+const UpdateInvoice = FormSchema.omit({ id:true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
