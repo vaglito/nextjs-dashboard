c4e883cc8166abef822547d718317573080334c7
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-19 23:48:27 -0500

    Refactor DashboardPage to remove unused latestInvoices fetch and wrap LatestInvoices in Suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,14 +1,17 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData, } from '../../lib/data';
-import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
- 
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "../../lib/data";
+import { Suspense } from "react";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
+
 export default async function DashboardPage() {
   //const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  //const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -29,13 +32,15 @@ export default async function DashboardPage() {
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> 
+        />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
