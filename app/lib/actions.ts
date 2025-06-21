863a0fa9b28eb9716d2e83d37933fe05f2d8de19
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-21 11:21:20 -0500

    Add error handling for createInvoice and updateInvoice functions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,10 +32,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0]; // Get current date in YYYY-MM-DD format
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error("Error in feching invoices:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -51,20 +55,25 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error("Error updating invoice:", error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   await sql`
     DELETE FROM invoices WHERE id = ${id};
   `;
+
   revalidatePath("/dashboard/invoices");
 }
