aeb89b823e2ebd50d78e379e38f9aedbc960d19f
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-21 16:57:36 -0500

    Add error handling to updateInvoice function for improved validation and feedback

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -75,13 +75,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to update invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`
@@ -90,7 +102,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (error) {
-    console.error("Error updating invoice:", error);
+    return {
+      message: "Database Error: Failed to update invoice.",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -98,7 +112,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  
   await sql`
     DELETE FROM invoices WHERE id = ${id};
   `;
